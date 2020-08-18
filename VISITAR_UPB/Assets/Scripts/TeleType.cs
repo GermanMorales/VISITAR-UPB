2c7c6c2e86da4adbb355178f0297388f8ba966b1
AnaBM5 <anabmojica@hotmail.com>
2020-08-18 13:19:12 -0500

    Text Update

---
+++ VISITAR_UPB/Assets/Scripts/TeleType.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TeleType : MonoBehaviour
+{
+    private TextMeshPro text;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        text = gameObject.GetComponent<TextMeshPro>() ?? gameObject.AddComponent<TextMeshPro>();
+
+        int totalVisibleCharacters = text.textInfo.characterCount;
+        int counter = 0;
+        
+        int visibleCount = counter % (totalVisibleCharacters + 1);
+        
+        while (visibleCount < totalVisibleCharacters)
+        {
+            visibleCount = counter % (totalVisibleCharacters + 1);
+            text.maxVisibleCharacters = visibleCount;
+           
+            counter++;
+            
+            yield return new WaitForSeconds(0.5f);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
