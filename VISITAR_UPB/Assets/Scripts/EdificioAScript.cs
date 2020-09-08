d6fa68ccf930489fc3ffe9093b05bd83e6754f86
German Morales <germanricardomorales99@gmail.com>
2020-09-08 17:15:22 -0500

    Arrow

---
+++ VISITAR_UPB/Assets/Scripts/EdificioAScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdificioAScript : MonoBehaviour
+{
+    public GameObject Arrow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Arrow.transform.rotation = Quaternion.Euler(20,40,180);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
