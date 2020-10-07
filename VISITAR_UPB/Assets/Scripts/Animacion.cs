f5834e6d624808d34de58357ebecded933ca61b8
AnaBM5 <anabmojica@hotmail.com>
2020-10-07 17:04:08 -0500

    UI Anchors

--- VISITAR_UPB/Assets/Scripts/Animacion.cs
+++ VISITAR_UPB/Assets/Scripts/Animacion.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Animacion : MonoBehaviour
 {
+    Animator anim;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim =  GetComponent<Animator>();
+    }
+
+    public void Detected()
+    {
+        anim.SetTrigger("Detected");
     }
 
     // Update is called once per frame
