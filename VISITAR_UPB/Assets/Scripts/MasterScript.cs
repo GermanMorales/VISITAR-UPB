4d5384ed35d00773bb51c985a7bc52fca6b59d3a
German Morales <germanricardomorales99@gmail.com>
2020-11-09 15:04:47 -0500

    Nuevos triggers

--- VISITAR_UPB/Assets/Scripts/MasterScript.cs
+++ VISITAR_UPB/Assets/Scripts/MasterScript.cs
@@ -5,32 +5,6 @@ using Vuforia;
 
 public class MasterScript : MonoBehaviour
 {
-    public GameObject EdificioA;
-    public GameObject EdificioB;
-    public GameObject EdificioC;
-    public GameObject EdificioD;
-    public GameObject EdificioE;
-    public GameObject EdificioF;
-    public GameObject EdificioG;
-    public GameObject EdificioH;
-    public GameObject EdificioI;
-    public GameObject EdificioJ;
-    public GameObject EdificioK;
-    public GameObject EdificioL;
-    public GameObject Porteria;
-    public GameObject Templo;
-    public GameObject Biblioteca;
-    public GameObject Fruteria;
-    public GameObject Hueco;
-    public GameObject Carretera;
-    public GameObject Plaza;
-    public GameObject Teatro;
-    public GameObject Parqueadero;
-    public GameObject Escaleras1;
-    public GameObject Escaleras2;
-    public GameObject Escaleras3;
-    public GameObject EscalerasFork;
-
     public string Current;
 
     DefaultTrackableEventHandler defaultTrackableHandler;
@@ -51,6 +25,7 @@ public class MasterScript : MonoBehaviour
 
     public void GetNameofObject()
     {
+
         Debug.Log(this.tag);
     }
 
