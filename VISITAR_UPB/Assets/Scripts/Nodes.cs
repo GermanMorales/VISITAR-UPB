dc868d1b3282b9b4c66da00435920ae523441d67
AnaBM5 <anabmojica@hotmail.com>
2020-11-11 16:05:57 -0500

    Scripts Update

---
+++ VISITAR_UPB/Assets/Scripts/Nodes.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Nodes
+{
+    public String nombre;
+    public Quaternion quaternion;
+}
---
+++ VISITAR_UPB/Assets/Scripts/UbicacionScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UbicacionScript : MonoBehaviour
+{
+    public GameObject Arrow;
+    [SerializeField]
+    private List<Nodes> m_Nodes = new List<Nodes> ();
+
+    public virtual List<Nodes> nodes
+    {
+        get
+        {
+            return m_Nodes;
+        }
+    }
+    
+    public void Found()
+    {
+        Debug.Log ("Found");
+    }
+
+    void changeArrowPosition()
+    {
+        //apunta hacia el primer nodo de la lista
+        Arrow.transform.rotation = m_Nodes[0].quaternion;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
