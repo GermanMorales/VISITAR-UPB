292622d188906878c705d56f93a331780bc6c53e
German Morales <germanricardomorales99@gmail.com>
2020-11-11 16:53:20 -0500

    Lets do that

--- VISITAR_UPB/Assets/Scripts/Node.cs
+++ VISITAR_UPB/Assets/Scripts/Node.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class currenttriggerscript : MonoBehaviour
+public class Node : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
